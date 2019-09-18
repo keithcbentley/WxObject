56b12361905f6640d0251cb309a1e615bbbc43a0
Keith Bentley <keithcbentley@msn.com>
2019-09-17 23:00:59 -0700

    Test two independent ui objects.

--- wxtest.py
+++ wxtest.py
@@ -16,14 +16,20 @@ if __name__ == '__main__':
         my_module = sys.modules[__name__]
         file = my_module.__file__
         own_dir = os.path.dirname(file)
-        xobj_file = os.path.join(own_dir, 'wx1.xml')
-        xobjects = wxobject.XObjects()
+        xobj_file1 = os.path.join(own_dir, 'wx1.xml')
+        xobj_file2 = os.path.join(own_dir, 'wx2.xml')
+        xobjects1 = wxobject.XObjects()
+        xobjects2 = wxobject.XObjects()
         # xobjects.output_codegen = True
-        xobj = xobj2.Xobj2(all_namespaces, xobjects)
+        ui1 = xobj2.Xobj2(all_namespaces, xobjects1)
+        ui2 = xobj2.Xobj2(all_namespaces, xobjects2)
         app = wx.App()
-        xobj.instantiate_from_file(xobj_file)
 
-        xobjects.main_frame.Show()
+        ui1.instantiate_from_file(xobj_file1)
+        ui2.instantiate_from_file(xobj_file2)
+
+        xobjects1.main_frame.Show()
+        xobjects2.main_frame.Show()
 
         app.MainLoop()
 
