23f88c20dffb0288f695e514712c9bedb5204ce1
Keith Bentley <keithcbentley@msn.com>
2019-09-18 01:52:22 -0700

    Make hello2 ui.

---
+++ hello2.py
@@ -0,0 +1,32 @@
+import sys
+import os
+
+import wx
+
+import xobj
+import wxobject
+
+wx_namespace = "http://namespace.xobj.org/xobj/wx"
+
+all_namespaces = {'': wx_namespace,
+                  }
+
+if __name__ == '__main__':
+    def main():
+        my_module = sys.modules[__name__]
+        file = my_module.__file__
+        own_dir = os.path.dirname(file)
+        file1 = os.path.join(own_dir, 'hello2.xml')
+        wxobjects1 = wxobject.WxObjects()
+        # xobjects.output_codegen = True
+        ui1 = xobj.XobjParser(all_namespaces, wxobjects1)
+        app = wx.App()
+
+        ui1.instantiate_from_file(file1)
+
+        wxobjects1.main_frame.Show()
+
+        app.MainLoop()
+
+
+    main()
