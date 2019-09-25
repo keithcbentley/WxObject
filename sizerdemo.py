0ec7265bdda39debceb3c96b313fbbdaf288dacf
Keith Bentley <keithcbentley@msn.com>
2019-09-25 00:54:42 -0700

    Add sizer demo.  More xsd.

--- sizerdemo.py
+++ sizerdemo.py
@@ -16,25 +16,20 @@ if __name__ == '__main__':
         def __init__(self):
             super().__init__()
             self.main_frame = None
-            self.child_frame = None
 
 
     def main():
         my_module = sys.modules[__name__]
         file = my_module.__file__
         own_dir = os.path.dirname(file)
-        file1 = os.path.join(own_dir, 'wx1.xml')
-#        file2 = os.path.join(own_dir, 'wx2.xml')
+        file1 = os.path.join(own_dir, 'sizerdemo.xml')
         ui = ThisUi()
         wxobjects = wxobject.WxObjects(ui)
         #wxobjects.output_codegen = True
         xobj_parser = xobj.XobjParser(all_namespaces, wxobjects)
         app = wx.App()
-
         xobj_parser.instantiate_from_file(file1)
-
         ui.main_frame.Show()
-        ui.child_frame.Show()
         app.MainLoop()
 
 
